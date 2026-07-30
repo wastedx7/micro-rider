26afd9e98596c3af86c20d001424b5fb732078e2
wastedx7 <wastedx7@gmail.com>
2026-07-30 17:17:25 +0530

    Initialize API Gateway project with Maven setup and basic structure

---
+++ api-gateway/src/main/java/com/micro/api_gateway/service/JwtService.java
@@ -0,0 +1,30 @@
+package com.micro.api_gateway.service;
+
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Service
+public class JwtService {
+    
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey);
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
